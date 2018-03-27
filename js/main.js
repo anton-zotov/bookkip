7d7eb0d0a62c89bb9823f39f712195a03bd1cb30
anton-zotov <anton.a.zotov@gmail.com>
2018-03-27 16:11:28 +0300

    -

--- js/main.js
+++ js/main.js
@@ -49,35 +49,39 @@ function DeleteTag(btn)
 function InitTags()
 {
 	function css(a) {
-	    var sheets = document.styleSheets, o = {};
-	    for (var i in sheets) {
-	        var rules = sheets[i].rules || sheets[i].cssRules;
-	        for (var r in rules) {
-	            if (a.is(rules[r].selectorText)) {
-	                o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
-	            }
-	        }
-	    }
-	    return o;
+		var sheets = document.styleSheets, o = {};
+		for (var i in sheets) {
+			try {
+				var rules = sheets[i].rules || sheets[i].cssRules;
+				for (var r in rules) {
+					if (a.is(rules[r].selectorText)) {
+						o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
+					}
+				}
+			} catch (e) {
+				
+			}
+		}
+		return o;
 	}
 
 	function css2json(css) {
-	    var s = {};
-	    if (!css) return s;
-	    if (css instanceof CSSStyleDeclaration) {
-	        for (var i in css) {
-	            if ((css[i]).toLowerCase) {
-	                s[(css[i]).toLowerCase()] = (css[css[i]]);
-	            }
-	        }
-	    } else if (typeof css == "string") {
-	        css = css.split("; ");
-	        for (var i in css) {
-	            var l = css[i].split(": ");
-	            s[l[0].toLowerCase()] = (l[1]);
-	        }
-	    }
-	    return s;
+		var s = {};
+		if (!css) return s;
+		if (css instanceof CSSStyleDeclaration) {
+			for (var i in css) {
+				if ((css[i]).toLowerCase) {
+					s[(css[i]).toLowerCase()] = (css[css[i]]);
+				}
+			}
+		} else if (typeof css == "string") {
+			css = css.split("; ");
+			for (var i in css) {
+				var l = css[i].split(": ");
+				s[l[0].toLowerCase()] = (l[1]);
+			}
+		}
+		return s;
 	}
 
 	function AddTag(holder,text)
@@ -113,7 +117,7 @@ function InitTags()
 					//l(data);
 					$('#tag-hint ul li').remove();
 					$.each(data, function(i, v) {
-					    $('#tag-hint ul').append('<li data="'+v.title_clear+'">' + v.title + '</li>');
+						$('#tag-hint ul').append('<li data="'+v.title_clear+'">' + v.title + '</li>');
 					}); 
 					var x = $(e.currentTarget).position().left;
 					var y = $(e.currentTarget).position().top + $(e.currentTarget).outerHeight() + 2;
@@ -182,7 +186,7 @@ function InitTags()
 			return false;
 		}
 		var unicode=e.charCode? e.charCode : e.keyCode;
-    	var actualkey=String.fromCharCode(unicode);
+		var actualkey=String.fromCharCode(unicode);
 		if (actualkey == ',')
 		{
 			TransformTextToTag(e.currentTarget);
